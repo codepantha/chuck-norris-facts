159b7e24d58778476036ca96807ab0b48257fa53
codepanther <pezechigozirim@gmail.com>
2022-04-03 18:52:47 +0100

    create Navbar component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from "react";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div>
+      <Navbar />
       <h1 className="text-secondary text-2xl font-body">Chucky</h1>
     </div>
   );
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { useLocation } from "react-router-dom";
+import { MdOutlineArrowBackIos } from 'react-icons/md/'
+import { AiTwotoneHome } from 'react-icons/ai';
+
+const Navbar = () => {
+  const location = useLocation();
+  return (
+    <nav className="h-24 bg-primary text-secondaryContrast font-body flex justify-start items-center px-4">
+      {location.pathname === "/" ? <AiTwotoneHome size={24} /> : <MdOutlineArrowBackIos size={24} />}
+      {location.pathname.slice(1)}
+    </nav>
+  );
+};
+
+export default Navbar;

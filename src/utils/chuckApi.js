dc52ca838650a03530e5a5a2e40479a08e0ec980
codepanther <pezechigozirim@gmail.com>
2022-04-04 12:00:26 +0100

    define methods fetchCategories and fetchRandomJokeInACategory

--- src/utils/chuckApi.js
+++ src/utils/chuckApi.js
@@ -11,3 +11,15 @@ export const searchJokes = async (query) => {
   const data = await res.json();
   return data.result
 };
+
+export const fetchCategories = async () => {
+  const res = await fetch('https://api.chucknorris.io/jokes/categories')
+  const categories = await res.json();
+  return categories;
+}
+
+export const fetchRandomJokeInACategory = async (category) => {
+  const res = await fetch(`https://api.chucknorris.io/jokes/random?category=${category}`);
+  const joke = await res.json();
+  return joke;
+}

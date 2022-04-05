f400ec37c6b4f9f0c7c4db288f0e65d6307d7836
codepanther <pezechigozirim@gmail.com>
2022-04-05 03:30:12 +0100

    persist selected theme to localStorage

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Joke from "./components/Joke";
 import Navbar from "./components/Navbar";
 
 function App() {
-  const [theme, setTheme] = useState('light');
+  const savedTheme = localStorage.getItem('theme');
+  const [theme, setTheme] = useState(savedTheme ? savedTheme : 'dark');
+
+  useEffect(() => {
+    localStorage.setItem('theme', theme);
+  }, [theme])
+  
   return (
     <main className={`font-body w-full min-h-screen overflow-x-hidden ${theme === 'light' ? 'dark' : 'light'}`}>
       <div className="dark:bg-slate-800 min-h-screen">

fc74e71c54f097941615eb471248ef3f4b5a399e
codepanther <pezechigozirim@gmail.com>
2022-04-05 01:13:07 +0100

    add shadow to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import norris from '../assets/chuck-norris.png';
 
 const Navbar = () => {
   return (
-    <nav className="h-24 bg-primary text-secondaryContrast flex justify-start items-center px-4 mb-4">
+    <nav className="h-24 bg-primary text-secondaryContrast flex justify-start items-center px-4 mb-4 shadow-lg">
       <img alt="logo" src={norris} />
       <p className="text-2xl ml-2">ChuckJokes</p>
     </nav>
